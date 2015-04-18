ca3b10bf8908f90f68f813a3bcad5fdca11bbb66
Fellipe Brito <fellipe@devlandia.net>
2015-04-18 08:08:21 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,11 @@ var gulp        = require('gulp'),
     browserSync = require('browser-sync');
 
 paths = {
-	sass:	    './src/css/**/*.scss',
+  sass:             './src/css/**/*.scss',
   html:             './src/*.html',
-	jade: 	    './src/*.jade',
-	images:     './src/img/*',
-	coffee:     './src/js/**/*.coffee',
+  jade:             './src/*.jade',
+  images:           './src/img/*',
+  coffee:           './src/js/**/*.coffee',
   jquery:           './node_modules/jquery/dist/jquery.min.js',
   bootstrap_js:     './node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
   bootstrap_css:    './node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss',
